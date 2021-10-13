bc2fb81f4495c0bb33d9f6ffa27aae120133f385
SergeyNevinskiy <88575089+SergeyNevinskiy@users.noreply.github.com>
2021-10-13 20:22:10 +0300

    Hw-2
    Hw-2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let num1 = +prompt("Введите первое число");
+let num2 = +prompt("Введите второе число");
+let add = num1 + num2;
+let sub = num1 - num2;
+let multi = num1 * num2;
+let dev = num1 / num2;
+
+console.log(add,sub,multi,dev);
